b6a908e7cd4a210106fe8b570d06871d0333ca6a
MuffinBarde <76058321+muffinbarde@users.noreply.github.com>
2023-02-11 14:54:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -258,8 +258,7 @@ jQuery(document).ready(function($) {
 		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
 		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
 		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
-		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
-		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'));
 		});
 				
 	};
